6d76af693067c58cbeb89eadb3cdd644e7e1ae4f
Ujjwal Kumar <ujjwalkumarjietcs@gmail.com>
2024-01-21 23:02:48 +0530

    Calculator

---
+++ Calculator/script.js
@@ -0,0 +1,43 @@
+// let input = document.getElementById('inputbox')
+// let buttons = document.querySelectorAll('button')
+
+// let string = "";
+// let arr = Array.from(buttons);
+// arr.forEach(buttons =>{
+//     addEventListener('click',(e) =>{
+//         if(e.target.innerHTML == '='){
+//             string = eval(string);
+//             input.value = string;
+//         }
+
+//         string += e.target.innerHTML;
+//         input.value = string;
+//     })
+// })
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        
+    })
+})
